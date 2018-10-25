c4fe856baf3c9831ddd899e014a6a7e446891de5
linyupark <linyupark@gmail.com>
2018-10-25 16:36:01 +0800

    页面切换动画加入

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <h1>WP4 + Typescript + Vue + Babel7 + Mobx</h1>
-    <router-view/>
+    <h1>CRH Demo</h1>
+      <div class="page-container">
+        <transition :name="pageTransitionName">
+          <router-view/>
+        </transition>
+      </div>
   </div>
 </template>
 
 <style lang="stylus" src="@assets/styles/global"></style>
+
+<script>
+import Vue from 'vue';
+import routerSlideTo from '@lib/vue-router-history';
+import Component from 'vue-class-component';
+
+@Component({
+  watch: {
+    $route(to, from) {
+      const direction = routerSlideTo({ to, from });
+      this.pageTransitionName = direction ? `page-${direction}` : '';
+    }
+  }
+})
+export default class AppRoot extends Vue {
+  pageTransitionName = '';
+}
+</script>
